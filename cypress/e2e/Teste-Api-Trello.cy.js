d149e0c295e162c8d3c4b8c85e8e93f97e95c218
Douglas Souza <douglas45308@gmail.com>
2023-07-03 22:28:13 -0300

    inclusão do cypress plugin api

--- cypress/e2e/Teste-Api-Trello.cy.js
+++ cypress/e2e/Teste-Api-Trello.cy.js
@@ -3,7 +3,7 @@ describe('Testes Automatizados Api Trello', () => {
 
     const apiKey = '5004433386400af25cff42a899de721b';
     const apiToken = 'ATTA716fa4d158439af4bf942ac77f871e7fcf4289a4d8e2548e52776a45d9d81f13A60291B7';
-    const nomeBoard = 'Souza' // Inserir nome do Board
+    const nomeBoard = 'Japao' // Inserir nome do Board
     //const url = 'https://api.trello.com/1/boards/?name='
     const url = 'https://api.trello.com'
     var id_List = '64a3086dfb5efaf2ac66eb27'
@@ -12,29 +12,32 @@ describe('Testes Automatizados Api Trello', () => {
     
 
     it('[POST] Cadastrar um board', () => {
-        cy.request({
+        cy.api({
             method: 'POST',
             url: `${url}/1/boards/?name=${nomeBoard}&key=${apiKey}&token=${apiToken}`
-            //headers: { 'Accept': 'aplication/json' },
+            
         }).then((response) => {
             expect(response.status).to.be.equal(200)
             const listIdFromResponse = response.body.id;
             cy.log('ID board:', listIdFromResponse);
-
+            cy.log(`Nome do board criado é ${nomeBoard}`)
+            cy.screenshot('[POST] Cadastrar um board')
         })
     });
 
-    it('[POST] Cadastrar um card', () => {
+    it.only('[POST] Cadastrar um card', () => {
         
-        cy.request({
+        cy.api({
             method: 'POST',    
             url: `${url}/1/cards?idList=${id_List}&key=${apiKey}&token=${apiToken}`,
             body: {
-                name: 'O Corvo'
+                name: 'pinguim',
             }
+        
 
         }).then((response) => {
             expect(response.status).to.be.equal(200)
+            cy.screenshot('[POST] Cadastrar um card')
         })
     });
 
@@ -45,17 +48,19 @@ describe('Testes Automatizados Api Trello', () => {
             //headers: { 'Accept-Language': 'en-us', },
         }).then((response) => {
             expect(response.status).to.be.equal(200)
+            cy.screenshot('[DELETE] Excluir card')
         })
 
     });
 
-    it.only('[DELETE] Excluir board', () => {
+    it('[DELETE] Excluir board', () => {
         cy.request({
             method: 'DELETE',
             url: `${url}/1/boards/${id_ExcluirBoard}?key=${apiKey}&token=${apiToken}`,
             headers: { 'Accept-Language': 'en-us', },
         }).then((response) => {
             expect(response.status).to.be.equal(200)
+            cy.screenshot('[DELETE] Excluir board')
         })
 
     });
